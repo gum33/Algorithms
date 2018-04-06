1c36346f2b68fc4c2bf52d72c112bc76affae4d6
GOOOD <gum33@hi.is>
2018-04-06 14:47:34 +0000

    Update

--- Treap.py
+++ Treap.py
@@ -14,9 +14,7 @@ class NodeT:
             self.right = None
             self.parent = None
             self.val = val
-            
             self.freq = random.uniform(0,1)
-            print(self.val, 'f: ', self.freq)
             
     def __str__(self):
          return 'value: %s, Priority: %s' % (self.val, self.freq)
@@ -25,8 +23,7 @@ class Treap:
     
     def __init__(self):
         self.root = None
-        self.size = 0
-    
+  
     def insert(self, val):
         if self.root is None:
             self.root = NodeT(val)
@@ -102,27 +99,32 @@ class Treap:
     #Treap search                   
     def search(self, val):
         if self.root is not None:
-            self._search(self.root, val)
-        else: return None
+            return self._search(self.root, val)
+        else: 
+            return None
         
     #Regular search but new freq provided.
     def _search(self,  node, val, depth=1):
+            #Found node, swap give new prio
             if val is node.val:
                 freq = random.uniform(0,1)
                 if freq > node.freq:
                     node.freq = freq
-                    if(freq > node.parent.freq):
+                    if node is self.root:
+                        return node, depth
+                    if (freq > node.parent.freq):
                         if(node is node.parent.right):
                             self.swapleft(node)
                         else: self.swapright(node)
-                return node.val
+                return node, depth
             
+            #Keep searching
             elif val < node.val:
                return self._search(node.left, val, depth+1)
             else:
                 return self._search(node.right, val, depth+1)
 
-    
+    #Simple print
     def printTree(self):
         if(self.root is not None):
             self._printTree(self.root)
@@ -135,8 +137,13 @@ class Treap:
             
 mytree = Treap()
 
-for i in range(1,21):
+for i in range(1,5):
    mytree.insert( i)
 
 mytree.printTree()
+print()
+print('Found %s, Depth: %s' %(mytree.search(4)))
+print()
+mytree.printTree()
+
 #mytree.search(10)
--- optcost.py
+++ optcost.py
@@ -14,33 +14,56 @@ class Node:
             self.val = val
             
     def __str__(self):
-        return 'value: ', self.val
+        return 'value: %s' %self.val
 
+#Classic simple binary tree
+class bTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._insert(self.root, val)
+       
+    def _insert(self, node, val): 
+        if(node.val < val):
+            if node.right is not None:
+                self._insert(node.right, val)     
+            else:
+                node.right = Node(val)
+                node.right.parent = node
+        else:
+            if node.left is not None:
+                self._insert(node.left, val)
+            else:
+                node.left = Node(val)
+                node.left.parent = node
+    #Search bTree                  
+    def search(self, val):
+        if self.root is not None:
+            return self._search(self.root, val)
+        else: 
+            return None
         
-def insert(root, value):
-    if not root:
-        return Node(value)
-    elif value < root.val:
-        root.left = insert(root.left, value)
-    else:
-        root.right = insert(root.right, value)
-    return root
-
-
-def search(root, value, depth=1):
-    if not root:
-        return 0, 0
-    elif root.val == value:
-        return depth, root.val
-    elif value < root.val:
-        return search(root.left, value, depth+1)
-    else:
-        return search(root.right, value, depth+1)
+    #Regular search
+    def _search(self,  node, val, depth=1):
+            #Found node, swap give new prio
+            if val is node.val:
+                return node, depth
+            #Keep searching
+            elif val < node.val:
+               return self._search(node.left, val, depth+1)
+            else:
+                return self._search(node.right, val, depth+1)
+        
 
 
 def optBinaryTree(f):
     n = len(f)
     OptCost = np.zeros((n,n), dtype=float)
+    R =  np.diag(range(1,n+1))
     FA = np.zeros((n,n), dtype=float)
     
     def OptimalSearchTree(f):
@@ -48,14 +71,7 @@ def optBinaryTree(f):
         for j in range(0,n):
             for i in range(j,-1,-1):
                 computeOptCost(i,j)
-        return OptCost
-    
-    def OptimalSearchTree2(f):
-        initF(f)
-        for i in range(n,-1,-1):
-            for j in range(i,n):
-                computeOptCost(i,j)
-        return OptCost
+        
     
     def initF(f):
         for i in range(0,n):
@@ -68,13 +84,18 @@ def optBinaryTree(f):
             tmp = OptCost[i,r-1] + OptCost[r+1,j]
             if OptCost[i,j] > tmp:
                 OptCost[i,j] = tmp
+                R[i,j] = r+1
         if OptCost[i,j] == 1000:
             OptCost[i,j] = 0
         
         OptCost[i,j] = OptCost[i,j] + FA[i,j]
-
-    OptCost = OptimalSearchTree2(f)
-    return OptCost
+        
+    OptimalSearchTree(f)
+    
+    def constructTree(R):
+        optTree = bTree()
+        
+    return OptCost, R
 
 #Gráðug smíð á tré fyrir lykla A[1..n] með tíðnir f
 def greedyTree(A, f):
@@ -90,6 +111,22 @@ if __name__ == '__main__':
     #Setjum inn tíðni fylkið sem við viljum reikna
     A = [1, 2, 3, 4, 5, 6]
     f = np.array([0.15, 0.1, 0.12, 0.03, 0.2, 0.4],dtype=float)
-    
+    a = Node(3)
+    xd = optBinaryTree(f)
+    f2 = np.array([0.5, 0.2, 0.3])
+    B = [1, 2, 3]
+    xd2 = optBinaryTree(f2)
+    print(xd2[1])
+    print(xd[0])
     gt = greedyTree(A,f)
     print(search(gt,5))
+    mytree = Treap()
+
+    for i in range(1,5):
+       mytree.insert( i)
+    
+    mytree.printTree()
+    print()
+    print('Found %s, Depth: %s' %(mytree.search(4)))
+    print()
+    mytree.printTree()
